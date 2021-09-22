5109c4f35a4067d4f5bdca925e4c1541979f07ad
KenwoodFox <kenwood364@gmail.com>
2021-09-22 12:05:41 -0400

    Create basic simulogger for lab 1

---
+++ python/simulogger/__main__.py
@@ -0,0 +1,24 @@
+import csv
+
+
+from random import seed
+from random import random
+from datetime import datetime
+
+
+def simulogger():
+    with open('simulated_dht_data.csv', 'w') as datafile:
+        datalogger = csv.writer(datafile,
+                                delimiter=',',
+                                quotechar='"',
+                                quoting=csv.QUOTE_MINIMAL)
+
+        datalogger.writerow(['Num', 'Time', 'Value'])
+
+        for i in range(100):
+            datalogger.writerow([i, datetime.now().microsecond, random() + i])
+
+
+if __name__ == '__main__':
+    seed(1)
+    simulogger()
