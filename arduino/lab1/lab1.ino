49e0ad984bf1ea6093980da06d03591f44e2565f
KenwoodFox <kenwood364@gmail.com>
2021-09-23 22:49:09 -0400

    Fix and clean lab1 demo code for full function

--- arduino/lab1/lab1.ino
+++ arduino/lab1/lab1.ino
@@ -24,62 +24,77 @@ volatile float dhtTemperature = 0;
 // Misc persistant data
 float sum = 0;
 
+// Flags
+bool checkdht = false;
+
 
 void setup() {
   // Start serial comms and make serial buffer
   Serial.begin(115200);
 
+  // Delay while host device establishes a link (Mostly for LabView being weird)
+  delay(1000);
+
   // Begin DHT listener
   dht.begin();
 
   // Initalize hardware inturrupts.
-  Timer1.initialize(200000);
+  Timer1.initialize(50000); // Every 50 ms
 
-  // Attach updateDHT to hw inturrupt
-  Timer1.attachInterrupt(updateDHT);
-
-  // Start updateDHT
-  Timer1.start();
+  // Attach raiseDHTFlag to hw inturrupt
+  Timer1.attachInterrupt(raiseDHTFlag);
 
   // Spit out MOTD
   // print out some information about the software we're running.
   Serial.print("Starting Team Gold LAB1 software. Using version "); Serial.println(VERSION);
   Serial.print("This software compiled on "); Serial.println(COMPILED_ON); Serial.println();
+
+  // Delay before starting tasks.
+  delay(200);
+
+  // Start all tasks.
+  // Start raiseDHTFlag.
+  Timer1.start();
 }
+
 
 /* Listen for inturrupts and change a volitalie
  * array of data with new values when appropriate
  */
-void updateDHT(void) {
-  // Read in dht data
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-
-  // If either number is NAN, the frame is invalid!
-  if (isnan(h) || isnan(t)) {
-    Serial.println("Got invalid frame."); // Debug!
-  } else {
-    Serial.println("Got valid frame."); // Debug!
-    // Valid frame data si not coppied
-    dhtHumidity = h;
-    dhtHumidity = t;
-  }
+void raiseDHTFlag(void) {
+  checkdht = true;
 }
 
 
 void loop() {
   // Slow loop
-  delay(200);
+  delay(50);
+
+  // If its time to check the DHT sesor
+  if (checkdht) {
+    // Read in dht data
+    float _h = dht.readHumidity();
+    float _t = dht.readTemperature();
+
+    // If either number is NAN, the frame is invalid!
+    if (isnan(_h) || isnan(_t)) {
+      //Serial.println("Got invalid frame."); // Debug!
+    } else {
+      //Serial.println("Got valid frame."); // Debug!
+      // Valid frame data is coppied.
+      dhtHumidity = _h;
+      dhtTemperature = _t;
+    }
+  }
 
   // Check if data changed (TODO: Replace with actual data checksum)
   if (dhtHumidity + dhtTemperature != sum) {
     // Send large serial frame
     Serial.print("H"); Serial.print(dhtHumidity); Serial.print(",");
-    Serial.print("T"); Serial.print(dhtTemperature); Serial.print(",\n");
+    Serial.print("T"); Serial.print(dhtTemperature); Serial.println(",");
 
     sum = dhtHumidity + dhtTemperature;
   } else { 
-    // For debugging only!
-    Serial.println("Nothing to send.");
+    //Serial.println("Nothing to send."); // Debug!
   }
 }
