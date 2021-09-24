8b96f8b34603cf4942ccd228b94ec49b26987cf7
KenwoodFox <kenwood364@gmail.com>
2021-09-24 13:56:23 -0400

    Flesh out most of a serial logger

--- python/serial_logger.py/__main__.py
+++ python/serial_logger.py/__main__.py
@@ -7,6 +7,25 @@ import serial
 
 
 if __name__ == '__main__':
+
+    myframe = 'H39.00,T31.80,\n'
+    print(myframe)
+    print(myframe.split(','))
+
     with serial.Serial('/dev/ttyACM0', 115200, timeout=1) as arduino:
-        while not KeyboardInterrupt:
-            print(arduino.readline())
+        while True:
+            try:
+                # Frame is one data frame
+                frame = arduino.readline()
+
+                # Split up frame
+                frame = frame.split(',')
+
+                humidy = frame[0]
+                temp = frame[1]
+
+                # Logging
+                print(f'Got new frame: {frame}')
+            except KeyboardInterrupt:
+                print('Exiting safely.')
+                exit()
