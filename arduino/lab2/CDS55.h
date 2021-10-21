b6e452791a166227c7df09d91220a15c498ad94b
KenwoodFox <kenwood364@gmail.com>
2021-10-21 14:32:32 -0400

    Replace lab1 tests and automated build

--- arduino/lab2/CDS55.h
+++ arduino/lab2/CDS55.h
@@ -22,6 +22,7 @@ class CDS55 {
             getRawValue(); // Populate value
 
             // Magic 4th order polynomial (Sucks a lot)
+            // Based purely on provided lab data.
             double luxvalue = 1020.881 - (19.60206 * value) + (0.1080073 * pow(value, 2)) - (0.0001971755 * pow(value, 3)) + (1.218544e-7* pow(value, 4));
             return luxvalue;
         };
