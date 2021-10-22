1cd32945a98239f20b6a5c52baf2c8ae028019ce
KenwoodFox <kenwood364@gmail.com>
2021-10-22 13:21:31 -0400

    Fix bad accuracy math

--- python/goldhist/goldhist.py
+++ python/goldhist/goldhist.py
@@ -4,6 +4,7 @@
 
 import csv
 import argparse
+import subprocess
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -66,6 +67,9 @@ class GoldHist:
         # Convert data list to np array, this does not include time tho, and should.
         self.data = np.asarray(self.data)
 
+        # Get version
+        self.software_version = self.get_git_revision_hash()
+
     def run(self):
         # Make a histogram using the data
         fig, ax = plt.subplots(2)
@@ -75,12 +79,17 @@ class GoldHist:
         median = np.median(self.data)
         mean = np.mean(self.data)
         sigma = self.data.std()
-        accuracy = abs((mean - self.args.std) / mean) * 100
+        accuracy = abs((mean - self.args.std) / self.args.std) * 100
         percision = ((2 * sigma) / self.args.std) * 100
 
         max = np.amax(self.data)
+        self.log.info(f"Found data max: {max}")
+
         data_points_to_reach_max = np.where(self.data == max)[0][0]
+        self.log.info(f"It took {data_points_to_reach_max} data points to reach max.")
         time_to_max = self.time_scale[data_points_to_reach_max] - self.time_scale[0]
+        # Need to find the time it takes to get to 60% of the max value
+
         time_const = time_to_max
 
         # This is really messy and needs to be cleaned up!!!
@@ -91,6 +100,12 @@ class GoldHist:
                 r'$\sigma=%.2f$' % (sigma, ),
                 r'$\mathrm{accuracy}=%.2f$' % (accuracy, ),
                 r'$\mathrm{percision}=%.2f$' % (percision, )))
+
+            ax[0].annotate(f'Std value: {self.args.std}',
+                            xy=(1.0, 0.8),
+                            xycoords='axes fraction',
+                            horizontalalignment='right',
+                            verticalalignment='top')
         elif self.args.t:
             annotationtext = '\n'.join((
                 r'$\mu=%.2f$' % (mu, ),
@@ -103,7 +118,7 @@ class GoldHist:
                 r'$\mathrm{median}=%.2f$' % (median, ),
                 r'$\sigma=%.2f$' % (sigma, )))
 
-        ax[0].annotate('Gold Standard, Team Gold, SNHU',
+        ax[0].annotate(f'Gold Standard, Team Gold, SNHU\nVersion: {self.software_version}',
                          xy=(1.0, 1.25),
                          xycoords='axes fraction',
                          horizontalalignment='right',
@@ -130,3 +145,21 @@ class GoldHist:
                 verticalalignment='top', bbox=props)
 
         plt.show()
+
+    def get_git_revision_hash(self) -> str:
+        return subprocess.check_output(['git', 'describe', '--dirty', '--abbrev=4', '--always', '--tags']).decode('ascii').strip()  # Noqa: E501
+
+    def find_nearest(self, list, target):
+        normalized_list = []
+
+        for element in list:
+            element = abs(target - element)
+            normalized_list.append(element)
+
+        local_min = 10
+        for element in normalized_list:
+            if element < local_min:
+                local_min = element
+
+        index_of_min = normalized_list.index(local_min)
+        return index_of_min
